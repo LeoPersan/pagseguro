ada9ace9602a608c0e9d27a79698ed43ae5b02af
Leonardo Persan <leopso1990@gmail.com>
2021-01-02 17:56:18 -0300

    feat: iniciar sessao

--- src/PagSeguro.php
+++ src/PagSeguro.php
@@ -2,10 +2,38 @@
 
 namespace LeoPersan;
 
+use GuzzleHttp\Client;
+use SimpleXMLElement;
+
 class PagSeguro
 {
-    public function home()
+    private $client = false;
+    private $email = false;
+    private $token = false;
+    private $session = false;
+
+    public function __construct($email, $token, $sandbox = false) {
+        $this->email = $email;
+        $this->token = $token;
+        $this->client = new Client([
+            'base_uri' => $sandbox ? 'https://ws.sandbox.pagseguro.uol.com.br/v2/' : 'https://ws.pagseguro.uol.com.br/v2/',
+        ]);
+    }
+
+    public function startSession()
     {
-        return 'deu certo';
+        $this->session = $this->post('sessions?email='.$this->email.'&token='.$this->token)->id;
+    }
+
+    public function post($uri, array $options = [])
+    {
+        return new SimpleXMLElement(
+            (string) $this->client->post($uri, $options)->getBody()
+        );
+    }
+
+    public function getSession()
+    {
+        return $this->session;
     }
 }
